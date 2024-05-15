2b2274acb4a6ec0d0e9bcf3885ec9a3fc686cdf7
Gabriel Muthenya <gabriel@bildit.co>
2024-05-15 14:58:05 +0300

    app controller

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,8 +1,20 @@
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
+/**
+ * AppController class contains methods to handle API requests
+ * related to the status and statistics of the application.
+ */
 class AppController {
-  // Gets the status of redis and db
+  /**
+   * getStatus - Gets the status of Redis and the database.
+   * This method checks if the Redis and database clients are alive
+   * and responds with their statuses.
+   *
+   * @param {Object} req - The request object (not used in this method).
+   * @param {Object} res - The response object used to send back the status.
+   * @returns {Promise<void>} - A promise that resolves when the response is sent.
+   */
   static async getStatus(req, res) {
     const redisIsAlive = redisClient.isAlive();
     const dbIsAlive = dbClient.isAlive();
@@ -10,7 +22,15 @@ class AppController {
     await res.status(200).json(status);
   }
 
-  // Gets the stats of of the users and files in DB
+  /**
+   * getStats - Gets the statistics of users and files in the database.
+   * This method retrieves the number of users and files from the database
+   * and responds with these statistics.
+   *
+   * @param {Object} req - The request object (not used in this method).
+   * @param {Object} res - The response object used to send back the statistics.
+   * @returns {Promise<void>} - A promise that resolves when the response is sent.
+   */
   static async getStats(req, res) {
     const usersCount = await dbClient.nbUsers();
     const filesCount = await dbClient.nbFiles();
